1c71f2335ca9d5bec0de2d128afbd65f8ecf46a0
Davidyoon <davidyoon@koreainvestment.com>
2023-05-03 11:36:29 +0900

    Initial Commit

---
+++ StarBucksSwiftUI/StarBucksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarBucksSwiftUIApp.swift
+//  StarBucksSwiftUI
+//
+//  Created by jiwon Yoon on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct StarBucksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
